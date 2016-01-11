32ed0156b5bbced8686e6421c3abf20b768d82b7
michbas796 <michbas796@users.noreply.github.com>
2016-01-11 20:12:41 +0100

    Kontrolka kalendarza w formularzu dla miotów

--- nowy_miot.php
+++ nowy_miot.php
@@ -20,11 +20,28 @@ if (empty($_SESSION['user'])) {
             }
             $stmt->execute();
         ?>
+        <style type="text/css"> @import url("//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css")</style>
+        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+        <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>  
+        <script type="text/javascript" src="datepicker-pl.js"></script>
+        <script>
+            $.datepicker.setDefaults(
+            $.extend(
+                {},
+                $.datepicker.regional["pl"],  
+                { dateFormat: "yy-mm-dd" } 
+            )
+          );
+            $(function() {
+              $( "#birthDate" ).datepicker();
+	      $( "#markupDate" ).datepicker();              
+            });
+        </script>
         <H1>Nowy miot:</H1><br>
         <div class="form-style">
             <form action="wstawianie_nowego_miotu.php" method="POST">                  
-                <label for="birthDate"><span>Data urodzenia:</span><input type="text" name="birthDate"></label>                
-                <label for="markupDate"><span>Data znakowania:</span><input type="text" name="markupDate"></label>                
+                <label for="birthDate"><span>Data urodzenia:</span><input type="text" name="birthDate" id="birthDate"></label>                
+                <label for="markupDate"><span>Data znakowania:</span><input type="text" name="markupDate" id="markupDate"></label>                
                 <label for="markupPosition"><span>Pozycja znakowania:</span><input type="text" name="markupPosition"></label>              
                 <label for="breeding"><span>Hodowla:</span> 
                     <select name="breeding"> 
@@ -37,5 +54,5 @@ if (empty($_SESSION['user'])) {
                 <label><span>&nbsp;</span><input type="submit" value="Dodaj"></label>                                   
             </form>
         </div>
+        
 <?php include "footer.php" ?> 
-
