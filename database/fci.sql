9a357e4f1a9f49a7c770cea806ff76dbd6575e96
Paweł Wilczek <5646246+beowoolf@users.noreply.github.com>
2016-02-02 21:58:27 +0100

    Create fci.sql

---
+++ database/fci.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 02 Lut 2016, 21:35
+-- Wersja serwera: 10.1.8-MariaDB
+-- Wersja PHP: 5.6.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `wds_di`
+--
+
+--
+-- Zrzut danych tabeli `fci`
+--
+
+INSERT INTO `fci` (`ID`, `NAZWA`) VALUES
+(907, 'angielskie spaniele miniaturowe'),
+(901, 'biszony i rasy pokrewne'),
+(1001, 'charty długowłose'),
+(1003, 'charty krótkowłose'),
+(1002, 'charty szorstkowłose'),
+(906, 'Chihuahua'),
+(401, 'jamniki'),
+(909, 'kontynentalne spaniele miniaturowe'),
+(910, 'Kromfohrländer'),
+(903, 'małe psy belgijskie'),
+(911, 'małe psy molosowate'),
+(202, 'molosowate'),
+(101, 'owczarki'),
+(602, 'posokowce'),
+(502, 'północne psy myśliwskie'),
+(503, 'północne psy pasterskie'),
+(501, 'północne psy zaprzęgowe'),
+(801, 'psy aportujące'),
+(904, 'psy bezwłose'),
+(803, 'psy dowodne'),
+(601, 'psy gończe'),
+(508, 'psy myśliwskie z pręgą na grzbiecie'),
+(201, 'psy w typie pinczera i sznaucera'),
+(102, 'psy zaganiające'),
+(902, 'pudle'),
+(802, 'płochacze'),
+(506, 'rasy pierwotne'),
+(507, 'rasy pierwotne myśliwskie'),
+(603, 'rasy pokrewne'),
+(905, 'rasy tybetańskie'),
+(908, 'spaniele japońskie i pekińczyk'),
+(505, 'szpice azjatyckie i rasy pokrewne'),
+(504, 'szpice europejskie'),
+(203, 'szwajcarskie psy pasterskie'),
+(301, 'teriery duże i średnie'),
+(302, 'teriery małe'),
+(304, 'teriery miniaturowe'),
+(303, 'teriery typu bull'),
+(702, 'wyżły brytyjskie i irlandzkie, setery'),
+(701, 'wyżły kontynentalne');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
